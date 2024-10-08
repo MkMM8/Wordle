6fa21373142cb45b796b99d60cf4be1b150c27e5
xenocrisis <akaxenonxss@gmail.com>
2024-10-08 14:03:27 +0200

    He creado la UI y la clase de card que tiene como metodos la actualizacion de su color en 4 estados diferentes y la actualizacion de letra.

--- wordle_game/lib/main.dart
+++ wordle_game/lib/main.dart
@@ -7,104 +7,97 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+          appBar: AppBar(title: const Text('Wordle')),
+          body: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 400.0),
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, // Centramos verticalmente el Column
+              children: [
+                Expanded(
+                  // Asegura que el CardsGrid use el espacio disponible
+                  child:
+                      CardsGrid(), // Incluye CardsGrid directamente en el Column
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CardsGrid extends StatefulWidget {
+  const CardsGrid({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CardsGridState createState() => _CardsGridState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CardsGridState extends State<CardsGrid> {
+  // Lista que almacena los colores iniciales de cada Card
+  final List<Color> _cardColors = List<Color>.generate(
+      30, (index) => const Color.fromARGB(255, 228, 230, 233));
 
-  void _incrementCounter() {
+  final List<String> _cardLetters = List<String>.generate(30, (index) => '');
+
+  // Método para cambiar dinámicamente el color de una tarjeta
+  void changeCardColor(int index, int state) {
+    var cardStates = {
+      0: const Color.fromARGB(255, 255, 255, 255),
+      1: const Color(0xFFa4aec4),
+      2: const Color(0xFFf3c237),
+      3: const Color(0xFF79b851),
+    };
+
+    if (cardStates.containsKey(state)) {
+      setState(() {
+        _cardColors[index] = cardStates[state]!;
+      });
+    } else {
+      //print('Error changing card states.');
+    }
+  }
+
+  void changeCardText(int index, String letter) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _cardLetters[index] = letter;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5,
+        crossAxisSpacing: 3,
+        mainAxisSpacing: 3,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      itemCount: _cardColors.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          // La funcion onTap es para testear.
+          onTap: () => {changeCardText(index, 'A')},
+          onDoubleTap: () => {changeCardColor(index, 2)},
+          child: Card(
+            color: _cardColors[index],
+            child: SizedBox(
+              child: Center(
+                child: Text(
+                  _cardLetters[index],
+                  maxLines: 1,
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(fontSize: 35)
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        );
+      },
     );
   }
 }
